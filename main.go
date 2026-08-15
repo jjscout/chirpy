49b05ae24dfc4360f72652ee954479cf584d039b
jjscout <evggalperin@gmail.com>
2026-08-15 19:06:32 +0300

    update api

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"sync/atomic"
+	"encoding/json"
+	"strings"
 )
 
 func handleHealthz(w http.ResponseWriter, request *http.Request) {
@@ -52,6 +54,75 @@ func (cfg *apiConfig) handleRequestsCount(w http.ResponseWriter, request *http.R
 	}
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type Errval struct {
+		Errstr string `json:"error"`
+	}
+	respondWithJSON(w, code, Errval{Errstr: msg})
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+			w.WriteHeader(500)
+			return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func replaceProfanities(message string, blacklist []string) string {
+
+	words := strings.Split(message, " ")
+	blackSet := make(map[string]struct{}, len(blacklist))
+	for _, word := range blacklist {
+		blackSet[strings.ToLower(word)] = struct{}{}
+	}
+	var filtered []string
+	for _, orw := range words {
+		lw := strings.ToLower(orw)
+		if _, found := blackSet[lw]; found {
+			filtered = append(filtered, "****")
+		} else {
+			filtered = append(filtered, orw)
+		}
+	}
+	return strings.Join(filtered, " ")
+}
+
+func (cfg *apiConfig) handleValidateChirp(w http.ResponseWriter, request *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type response struct {
+		Body string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(request.Body)
+	p := parameters{}
+	err := decoder.Decode(&p)
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("error: %v", err))
+		return
+	}
+	if len(p.Body) > 140 {
+		respondWithError(w, 400, "Chirp too long")
+		return
+	}
+
+	filtered := replaceProfanities(
+		p.Body,
+		[]string{
+			"kerfuffle",
+			"sharbert",
+			"fornax",
+		},
+	)
+	respondWithJSON(w, 200, response{Body: filtered})
+}
+
 func main() {
 
 	apiCfg := apiConfig{}
@@ -65,6 +136,7 @@ func main() {
 	serveMux.HandleFunc("GET /api/healthz", handleHealthz)
 	serveMux.HandleFunc("GET /admin/metrics", apiCfg.handleRequestsCount)
 	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	serveMux.HandleFunc("POST /api/validate_chirp", apiCfg.handleValidateChirp)
 	serveMux.Handle(
 		"/app/",
 		apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))),
